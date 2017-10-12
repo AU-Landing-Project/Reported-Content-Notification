0a8f56204711d6053f30ea72e4750019be0f9ef3
Steve Clay <steve@mrclay.org>
2017-10-12 16:59:07 -0400

    Fix notify users update and allow usernames

--- languages/en.php
+++ languages/en.php
@@ -2,8 +2,8 @@
 
 $english = array(
     'reportedcontent_notify' => "Reported Content Notification",
-    'reportedcontent_notify:description' => "Emails to notify",
-	'reportedcontent_notify:description:help' => "Enter one email per line.  These emails will be notified when content is reported.",
+    'reportedcontent_notify:description' => "Users to notify (username or email)",
+	'reportedcontent_notify:description:help' => "Enter one username/email per line. These emails will be notified when content is reported.",
     'reportedcontent_notify:message' => "Some content has been reported:
       
 %s
--- start.php
+++ start.php
@@ -8,37 +8,56 @@ const PLUGIN_VERSION = 20150903;
 elgg_register_event_handler('init', 'system', __NAMESPACE__ . '\\init');
 
 function init(){
-  // register plugin hook handler to send notifications
-  elgg_register_plugin_hook_handler('reportedcontent:add', 'system', __NAMESPACE__ . '\\reported_content_created');
-  
-  elgg_register_event_handler('upgrade', 'system', __NAMESPACE__ . '\\upgrades');
+	// register plugin hook handler to send notifications
+	elgg_register_plugin_hook_handler('reportedcontent:add', 'system', __NAMESPACE__ . '\\reported_content_created');
+
+	elgg_register_event_handler('upgrade', 'system', __NAMESPACE__ . '\\upgrades');
+}
+
+function reported_content_get_users_to_notify() {
+	$recipients_email_str = elgg_get_plugin_setting('notify_list', 'reportedcontent_notify');
+	$searches = preg_split('~\s+~', $recipients_email_str, -1, PREG_SPLIT_NO_EMPTY);
+
+	$map = function ($search) {
+		$search = trim($search);
+		if (!$search) {
+			return false;
+		}
+
+		$users = get_user_by_email($search);
+		if ($users) {
+			return $users[0];
+		}
+
+		$user = get_user_by_username($search);
+		return $user;
+	};
+	$users = array_map($map, $searches);
+	$users = array_filter($users, function ($user) {
+		return $user instanceof \ElggUser;
+	});
+
+	return $users;
 }
 
 // this triggers when content is reported
 function reported_content_created($hook, $type, $returnvalue, $params){
-  $recipients_list = elgg_get_plugin_setting('recipients', 'reportedcontent_notify');
-  
-  if(!empty($recipients_list)){
-    $recipients = unserialize($recipients_list);
-    
-    foreach($recipients as $guid){
-      notify_user(
-              $guid,
-              $guid,
-              elgg_echo('reportedcontent_notify:subject', array($params['report']->title)),
-              elgg_echo('reportedcontent_notify:message', array($params['report']->description, $params['report']->address))
-      );
-    }
-  }
+	foreach (reported_content_get_users_to_notify() as $user) {
+		notify_user(
+			$user->guid,
+			$user->guid,
+			elgg_echo('reportedcontent_notify:subject', array($params['report']->title)),
+			elgg_echo('reportedcontent_notify:message', array($params['report']->description, $params['report']->address))
+		);
+	}
 }
 
 function upgrades() {
 	if (!elgg_is_admin_logged_in()) {
 		return;
 	}
-	
+
 	require_once __DIR__ . '/lib/upgrades.php';
-	
+
 	run_function_once(__NAMESPACE__ . '\\upgrade20150930');
 }
-
